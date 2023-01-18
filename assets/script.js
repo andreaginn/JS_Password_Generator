37ea775fc7a16562d4596c7c3fb60aeba8ba91c7
Rose Ginn <andrearginn@gmail.com>
2023-01-17 21:55:50 -0800

    fixed error- missing curly brace

--- assets/script.js
+++ assets/script.js
@@ -4,8 +4,6 @@ var lowercase = 'abcdefghijklmnopqrstuvwxyz';
 var numbers = '1234567890';
 var symbols = '!@#$%^&*(){}';
 
-
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
@@ -42,13 +40,7 @@ function generatePassword() {
     alert("Please make sure your selection is between 8 and 128")
   )
   return chosenPassword;
-
-
-
-
-
-
-
+}
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -56,6 +48,5 @@ function writePassword() {
   passwordText.value = password;
 
 }
-
-// Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
