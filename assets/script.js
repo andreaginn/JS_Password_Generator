a48f7c761951295c308a88968bc86d1b41d88fb1
Rose Ginn <andrearginn@gmail.com>
2023-01-17 21:19:29 -0800

    added prompt questions

--- assets/script.js
+++ assets/script.js
@@ -10,6 +10,20 @@ var symbols = '!@#$%^&*(){}';
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
+function generatePassword() {
+  var chosenPassword = '';
+  var passwordSize = prompt("Please pick a password length between 8 and 128 characters");
+  var uppercaseOption = confirm("Include uppercase characters in password?");
+  var lowercaseOption = confirm("Include lowercase characters in password?");
+  var numbersOption = confirm("Include numbers in password?");
+  var symbolsOption = confirm("Include symbols in password?");
+
+
+
+
+
+
+
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
